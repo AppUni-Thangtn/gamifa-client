12b4ac2119fbaf37bb24e194688e97515c5bfcb5
thangtrinh <thang.tn@appuni.io>
2023-11-29 16:22:50 +0700

    feat: meta twitter

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,20 +6,19 @@ import Head from 'next/head';
 const inter = Inter({ subsets: ['latin'] });
 
 export const getServerSideProps: GetServerSideProps = async (context: any) => {
-  const host = context.req.headers.host;
-
+  const host = context.req.headers.host || 'staging.gamifa.com';
   const url = `https://${host}`;
 
   const res = await fetch(
-    `https://staging-api.gamifa.vn/api/channel/list?domain=${url}`
+    `${process.env.SERVER_URL}/channel/list?domain=${url}`
   );
   const channelData = (await res.json())[0];
   console.log('data', channelData);
 
   return {
     props: {
-      host: host,
       fonts: inter,
+      host: host,
       title: channelData.name,
       icon: channelData.avatar.media_url,
       shortDescription: channelData.short_description,
@@ -46,6 +45,11 @@ export default function Home({
         <meta property="og:title" content={title} key="title" />
         <meta property="og:image" content={icon} />
         <meta property="og:image:alt" content={short_description} />
+
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image:src" content={icon} />
+        <meta name="twitter:image:alt" content={short_description} />
       </Head>
       <main
         className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
